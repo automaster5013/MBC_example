dc41f836f8f88a028121228751acab11aa6477d1
myeonghwi <automaster5013@gmail.com>
2026-06-19 13:14:46 +0900

    JAVA_연습문제_예제풀이

--- src/d4/p66/DateTest.java
+++ src/d4/p66/DateTest.java
@@ -9,7 +9,7 @@ public class DateTest {
         String strNow1 = now.toString();
         System.out.println(strNow1);
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 mm윌 dd일 hh시 mm분 ss초");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM윌 dd일 hh시 mm분 ss초");
         String strNow2 = sdf.format(now);
         System.out.println(strNow2);
     }
